8bcf88786b0f361888ab10bd01f935b2dad1dc91
bipblop555 <tom.bonnet@hotmail.com>
2022-11-26 15:43:06 +0100

    ajout css

--- app/views/login.php
+++ app/views/login.php
@@ -1,8 +1,16 @@
-<?php
-echo 'Login';
-?>
-<form action="" method="POST">
-    <input type ="text" name="username" placeholder="username"/>
-    <input type="password" name ="password" placeholder="password"/>
-    <button type="submit">submit</button>
-</form>
+<div class="log_wrapper">
+    <div class="log_div">
+        <form action="" method="POST" class="log_form">
+            <p>Login</p>
+            <input type ="text" name="username" placeholder="username"/>
+            <input type="password" name ="password" placeholder="password"/>
+            <button type="submit">submit</button>
+            <span>No account yet ? <a href="register.php">Subscribe now !</a></span>
+        </form>
+    </div>
+    <div class="pic_div">
+        <figure>
+            <img src='https://cdn.shopify.com/s/files/1/0070/7032/files/how-to-start-a-blog-illustration.png?format=jpg&quality=90&v=1595363254'/>
+        </figure>
+    </div>
+</div>
--- app/views/register.php
+++ app/views/register.php
@@ -1,5 +1,25 @@
-<form action="" method="post">
+
+<!-- <form method="POST">
     <input type ="text" name="username" placeholder="username"/>
     <input type="password" name ="password" placeholder="password"/>
     <button class="button is-primary">Envoyer !</button>
-</form>
+</form>  -->
+
+
+<div class="register_wrapper">
+    <div class="register_div">
+        <form action="" method="POST" class="register_form">
+            <p>register</p>
+            <input type ="text" name="username" placeholder="username"/>
+            <input type="password" name ="password" placeholder="password"/>
+
+            <button type="submit">submit</button>
+            <!-- <span>No account yet ? <a href="register.php">Subscribe now !</a></span> -->
+        </form>
+    </div>
+    <div class="pic_div">
+        <figure>
+            <img src='https://cdn.shopify.com/s/files/1/0070/7032/files/how-to-start-a-blog-illustration.png?format=jpg&quality=90&v=1595363254'/>
+        </figure>
+    </div>
+</div>
